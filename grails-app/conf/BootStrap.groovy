df189b91c60f085018cc418e9fdf1ced826206f5
Dean Macaulay <dean.macaulay@finance.gov.au>
2015-03-16 17:18:18 +1100

    now creates subscriber, subscription and agencies on boot

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,6 @@
 import subscriberpoc.Agency
+import subscriberpoc.Subscriber
+import subscriberpoc.Subscription
 
 class BootStrap {
 
@@ -6,7 +8,7 @@ class BootStrap {
         environments {
             development {
 
-                if(!Agency.count()) createSampleData()
+                if(!Agency.count() && (!Subscriber.count())) createSampleData()
             }
         }
     }
@@ -16,7 +18,8 @@ class BootStrap {
     private createSampleData(){
 
         new Agency(title: "Dept of Pork Rolls", portfolio: "Food").save()
-        new Agency(title: "Dept of Finance", portfolio: "Finance", urls: ["http://finance.gov.au/about-the-department/media-centre","http://financeminister.gov.au/media/2015/index.html"]).save()
-
+        new Agency(title: "Dept of Finance", portfolio: "Finance", urls: ["http://www.finance.gov.au/about-the-department/media-centre","http://www.financeminister.gov.au/media/2015/index.html"]).save()
+        new Subscriber(name: "Pablo", email: "pablo@test.com").save()
+        new Subscription(subscriber: Subscriber.findByName("Pablo"), agencies: Agency.findByTitle("Dept of Pork Rolls")).save()
     }
 }
