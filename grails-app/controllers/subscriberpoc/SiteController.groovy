54da3ecfa08fad219abb76c90913213aa61dc53b
Dean Macaulay <dean.macaulay@finance.gov.au>
2015-03-23 10:46:15 +1100

    Added logging. To use add private static final log = LogFactory.getLog(this) to class, then add log.debug when you want to see it.

--- grails-app/controllers/subscriberpoc/SiteController.groovy
+++ grails-app/controllers/subscriberpoc/SiteController.groovy
@@ -1,8 +1,11 @@
 package subscriberpoc
 
 import grails.rest.RestfulController
+import org.apache.commons.logging.LogFactory
 
 class SiteController extends RestfulController {
+
+    private static final log = LogFactory.getLog(this)
 
     static scaffold = true
     static responseFormats = ['html', 'json', 'xml']
