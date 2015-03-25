8e4e31b5b5868f2bdce440134452b078b7260a09
Dean Macaulay <dean@magpies.net>
2015-03-25 13:44:52 +1100

    Secured remaining controllers

--- grails-app/controllers/subscriberpoc/ReleaseController.groovy
+++ grails-app/controllers/subscriberpoc/ReleaseController.groovy
@@ -1,9 +1,11 @@
 package subscriberpoc
 
 import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
 import grails.rest.RestfulController
 import org.apache.commons.logging.LogFactory
 
+@Secured(['ROLE_ADMIN'])
 class ReleaseController extends RestfulController {
 
     private static final log = LogFactory.getLog(this)
