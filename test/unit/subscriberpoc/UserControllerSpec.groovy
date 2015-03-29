9b9516d9be5b24d4c2e46cb16126cd042cebf48c
Dean Macaulay <dean@magpies.net>
2015-03-30 10:32:23 +1100

    Updated user test removing reference to subscriber

--- test/unit/subscriberpoc/UserControllerSpec.groovy
+++ test/unit/subscriberpoc/UserControllerSpec.groovy
@@ -6,7 +6,7 @@ import grails.test.mixin.*
 import spock.lang.*
 
 @TestFor(UserController)
-@Mock(Subscriber)
+@Mock(User)
 class UserControllerSpec extends Specification {
 
     def populateValidParams(params) {
@@ -21,8 +21,8 @@ class UserControllerSpec extends Specification {
             controller.index()
 
         then:"The model is correct"
-            !model.subscriberInstanceList
-            model.subscriberInstanceCount == 0
+            !model.userInstanceList
+            model.userInstanceCount == 0
     }
 
     void "Test the create action returns the correct model"() {
@@ -30,7 +30,7 @@ class UserControllerSpec extends Specification {
             controller.create()
 
         then:"The model is correctly created"
-            model.subscriberInstance!= null
+            model.userInstance!= null
     }
 
     void "Test the save action correctly persists an instance"() {
@@ -38,25 +38,25 @@ class UserControllerSpec extends Specification {
         when:"The save action is executed with an invalid instance"
             request.contentType = FORM_CONTENT_TYPE
             request.method = 'POST'
-            def subscriber = new Subscriber()
-            subscriber.validate()
-            controller.save(subscriber)
+            def user = new User()
+            user.validate()
+            controller.save(user)
 
         then:"The create view is rendered again with the correct model"
-            model.subscriberInstance!= null
+            model.userInstance!= null
             view == 'create'
 
         when:"The save action is executed with a valid instance"
             response.reset()
             populateValidParams(params)
-            subscriber = new Subscriber(params)
+            user = new User(params)
 
-            controller.save(subscriber)
+            controller.save(user)
 
         then:"A redirect is issued to the show action"
-            response.redirectedUrl == '/subscriber/show/1'
+            response.redirectedUrl == '/user/show/1'
             controller.flash.message != null
-            Subscriber.count() == 1
+            User.count() == 1
     }
 
     void "Test that the show action returns the correct model"() {
@@ -68,11 +68,11 @@ class UserControllerSpec extends Specification {
 
         when:"A domain instance is passed to the show action"
             populateValidParams(params)
-            def subscriber = new Subscriber(params)
-            controller.show(subscriber)
+            def user = new User(params)
+            controller.show(user)
 
         then:"A model is populated containing the domain instance"
-            model.subscriberInstance == subscriber
+            model.userInstance == user
     }
 
     void "Test that the edit action returns the correct model"() {
@@ -84,11 +84,11 @@ class UserControllerSpec extends Specification {
 
         when:"A domain instance is passed to the edit action"
             populateValidParams(params)
-            def subscriber = new Subscriber(params)
-            controller.edit(subscriber)
+            def user = new User(params)
+            controller.edit(user)
 
         then:"A model is populated containing the domain instance"
-            model.subscriberInstance == subscriber
+            model.userInstance == user
     }
 
     void "Test the update action performs an update on a valid domain instance"() {
@@ -98,28 +98,28 @@ class UserControllerSpec extends Specification {
             controller.update(null)
 
         then:"A 404 error is returned"
-            response.redirectedUrl == '/subscriber/index'
+            response.redirectedUrl == '/user/index'
             flash.message != null
 
 
         when:"An invalid domain instance is passed to the update action"
             response.reset()
-            def subscriber = new Subscriber()
-            subscriber.validate()
-            controller.update(subscriber)
+            def user = new User()
+            user.validate()
+            controller.update(user)
 
         then:"The edit view is rendered again with the invalid instance"
             view == 'edit'
-            model.subscriberInstance == subscriber
+            model.userInstance == user
 
         when:"A valid domain instance is passed to the update action"
             response.reset()
             populateValidParams(params)
-            subscriber = new Subscriber(params).save(flush: true)
-            controller.update(subscriber)
+            user = new User(params).save(flush: true)
+            controller.update(user)
 
         then:"A redirect is issues to the show action"
-            response.redirectedUrl == "/subscriber/show/$subscriber.id"
+            response.redirectedUrl == "/user/show/$user.id"
             flash.message != null
     }
 
@@ -130,23 +130,23 @@ class UserControllerSpec extends Specification {
             controller.delete(null)
 
         then:"A 404 is returned"
-            response.redirectedUrl == '/subscriber/index'
+            response.redirectedUrl == '/user/index'
             flash.message != null
 
         when:"A domain instance is created"
             response.reset()
             populateValidParams(params)
-            def subscriber = new Subscriber(params).save(flush: true)
+            def user = new User(params).save(flush: true)
 
         then:"It exists"
             Subscriber.count() == 1
 
         when:"The domain instance is passed to the delete action"
-            controller.delete(subscriber)
+            controller.delete(user)
 
         then:"The instance is deleted"
-            Subscriber.count() == 0
-            response.redirectedUrl == '/subscriber/index'
+        User.count() == 0
+            response.redirectedUrl == '/user/index'
             flash.message != null
     }
 }
