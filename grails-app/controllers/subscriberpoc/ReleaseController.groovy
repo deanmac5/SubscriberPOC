fd508f33ae9950f5dd18f090b49e1bd037eda1a6
Dean Macaulay <dean@magpies.net>
2015-03-30 14:53:23 +1100

    Generate new ReleaseController so that we can add granular permissions

--- grails-app/controllers/subscriberpoc/ReleaseController.groovy
+++ grails-app/controllers/subscriberpoc/ReleaseController.groovy
@@ -1,18 +1,108 @@
 package subscriberpoc
 
-import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
-import grails.rest.RestfulController
-import org.apache.commons.logging.LogFactory
 
-@Secured(['ROLE_ADMIN'])
-class ReleaseController extends RestfulController {
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
 
-    private static final log = LogFactory.getLog(this)
+@Transactional(readOnly = true)
+class ReleaseController {
 
-    static scaffold = true
-    static responseFormats = ['html', 'json', 'xml']
-    ReleaseController() {
-        super(Release)
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Release.list(params), model: [releaseInstanceCount: Release.count()]
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def show(Release releaseInstance) {
+        respond releaseInstance
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def create() {
+        respond new Release(params)
+    }
+
+    @Transactional
+    def save(Release releaseInstance) {
+        if (releaseInstance == null) {
+            notFound()
+            return
+        }
+
+        if (releaseInstance.hasErrors()) {
+            respond releaseInstance.errors, view: 'create'
+            return
+        }
+
+        releaseInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'release.label', default: 'Release'), releaseInstance.id])
+                redirect releaseInstance
+            }
+            '*' { respond releaseInstance, [status: CREATED] }
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def edit(Release releaseInstance) {
+        respond releaseInstance
+    }
+
+    @Transactional
+    def update(Release releaseInstance) {
+        if (releaseInstance == null) {
+            notFound()
+            return
+        }
+
+        if (releaseInstance.hasErrors()) {
+            respond releaseInstance.errors, view: 'edit'
+            return
+        }
+
+        releaseInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Release.label', default: 'Release'), releaseInstance.id])
+                redirect releaseInstance
+            }
+            '*' { respond releaseInstance, [status: OK] }
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    @Transactional
+    def delete(Release releaseInstance) {
+
+        if (releaseInstance == null) {
+            notFound()
+            return
+        }
+
+        releaseInstance.delete flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Release.label', default: 'Release'), releaseInstance.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'release.label', default: 'Release'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NOT_FOUND }
+        }
     }
 }
--- test/unit/subscriberpoc/ReleaseControllerSpec.groovy
+++ test/unit/subscriberpoc/ReleaseControllerSpec.groovy
@@ -1,20 +1,152 @@
 package subscriberpoc
 
-import grails.test.mixin.TestFor
-import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
+
+import grails.test.mixin.*
+import spock.lang.*
+
 @TestFor(ReleaseController)
+@Mock(Release)
 class ReleaseControllerSpec extends Specification {
 
-    def setup() {
+    def populateValidParams(params) {
+        assert params != null
+        // TODO: Populate valid properties like...
+        //params["name"] = 'someValidName'
     }
 
-    def cleanup() {
+    void "Test the index action returns the correct model"() {
+
+        when:"The index action is executed"
+            controller.index()
+
+        then:"The model is correct"
+            !model.releaseInstanceList
+            model.releaseInstanceCount == 0
     }
 
-    void "test something"() {
+    void "Test the create action returns the correct model"() {
+        when:"The create action is executed"
+            controller.create()
+
+        then:"The model is correctly created"
+            model.releaseInstance!= null
+    }
+
+    void "Test the save action correctly persists an instance"() {
+
+        when:"The save action is executed with an invalid instance"
+            request.contentType = FORM_CONTENT_TYPE
+            request.method = 'POST'
+            def release = new Release()
+            release.validate()
+            controller.save(release)
+
+        then:"The create view is rendered again with the correct model"
+            model.releaseInstance!= null
+            view == 'create'
+
+        when:"The save action is executed with a valid instance"
+            response.reset()
+            populateValidParams(params)
+            release = new Release(params)
+
+            controller.save(release)
+
+        then:"A redirect is issued to the show action"
+            response.redirectedUrl == '/release/show/1'
+            controller.flash.message != null
+            Release.count() == 1
+    }
+
+    void "Test that the show action returns the correct model"() {
+        when:"The show action is executed with a null domain"
+            controller.show(null)
+
+        then:"A 404 error is returned"
+            response.status == 404
+
+        when:"A domain instance is passed to the show action"
+            populateValidParams(params)
+            def release = new Release(params)
+            controller.show(release)
+
+        then:"A model is populated containing the domain instance"
+            model.releaseInstance == release
+    }
+
+    void "Test that the edit action returns the correct model"() {
+        when:"The edit action is executed with a null domain"
+            controller.edit(null)
+
+        then:"A 404 error is returned"
+            response.status == 404
+
+        when:"A domain instance is passed to the edit action"
+            populateValidParams(params)
+            def release = new Release(params)
+            controller.edit(release)
+
+        then:"A model is populated containing the domain instance"
+            model.releaseInstance == release
+    }
+
+    void "Test the update action performs an update on a valid domain instance"() {
+        when:"Update is called for a domain instance that doesn't exist"
+            request.contentType = FORM_CONTENT_TYPE
+            request.method = 'PUT'
+            controller.update(null)
+
+        then:"A 404 error is returned"
+            response.redirectedUrl == '/release/index'
+            flash.message != null
+
+
+        when:"An invalid domain instance is passed to the update action"
+            response.reset()
+            def release = new Release()
+            release.validate()
+            controller.update(release)
+
+        then:"The edit view is rendered again with the invalid instance"
+            view == 'edit'
+            model.releaseInstance == release
+
+        when:"A valid domain instance is passed to the update action"
+            response.reset()
+            populateValidParams(params)
+            release = new Release(params).save(flush: true)
+            controller.update(release)
+
+        then:"A redirect is issues to the show action"
+            response.redirectedUrl == "/release/show/$release.id"
+            flash.message != null
+    }
+
+    void "Test that the delete action deletes an instance if it exists"() {
+        when:"The delete action is called for a null instance"
+            request.contentType = FORM_CONTENT_TYPE
+            request.method = 'DELETE'
+            controller.delete(null)
+
+        then:"A 404 is returned"
+            response.redirectedUrl == '/release/index'
+            flash.message != null
+
+        when:"A domain instance is created"
+            response.reset()
+            populateValidParams(params)
+            def release = new Release(params).save(flush: true)
+
+        then:"It exists"
+            Release.count() == 1
+
+        when:"The domain instance is passed to the delete action"
+            controller.delete(release)
+
+        then:"The instance is deleted"
+            Release.count() == 0
+            response.redirectedUrl == '/release/index'
+            flash.message != null
     }
 }
