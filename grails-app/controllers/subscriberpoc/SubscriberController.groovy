7bf8c9ff318acb2b42155ffbe46a44c96ba7fe46
Dean Macaulay <dean@magpies.net>
2015-05-12 11:30:53 +1000

    Added security to subscriber controller

--- grails-app/controllers/subscriberpoc/SubscriberController.groovy
+++ grails-app/controllers/subscriberpoc/SubscriberController.groovy
@@ -1,5 +1,8 @@
 package subscriberpoc
 
+import org.apache.commons.logging.LogFactory
+import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.plugin.springsecurity.annotation.Secured
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
@@ -7,13 +10,23 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class SubscriberController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static defaultAction = "create"
 
+    private static final log = LogFactory.getLog(this)
+
+    def springSecurityService
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static responseFormats = ['html', 'json', 'xml']
+
+    @Secured(['ROLE_USER', 'ROLE_ADMIN'])
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond Subscriber.list(params), model: [subscriberInstanceCount: Subscriber.count()]
     }
 
+
+    @Secured(['ROLE_USER', 'ROLE_ADMIN'])
     def show(Subscriber subscriberInstance) {
         respond subscriberInstance
     }
@@ -45,10 +58,12 @@ class SubscriberController {
         }
     }
 
+    @Secured(['ROLE_USER', 'ROLE_ADMIN'])
     def edit(Subscriber subscriberInstance) {
         respond subscriberInstance
     }
 
+    @Secured(['ROLE_USER', 'ROLE_ADMIN'])
     @Transactional
     def update(Subscriber subscriberInstance) {
         if (subscriberInstance == null) {
@@ -72,6 +87,7 @@ class SubscriberController {
         }
     }
 
+    @Secured(['ROLE_ADMIN'])
     @Transactional
     def delete(Subscriber subscriberInstance) {
 
